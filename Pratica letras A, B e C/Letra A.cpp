9ccc93c005bc1554d08e08f27be120933801f66b
FranciscaGeovanna <115251617+FranciscaGeovanna@users.noreply.github.com>
2022-12-20 14:39:33 -0300

    Add files via upload

---
+++ Pratica letras A, B e C/Letra A.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <locale.h>
+
+int soma (int n1, int n2){
+	return n1 + n2;
+}
+
+int main (){
+	setlocale (LC_ALL, "portuguese_Brazil");
+	
+	int a, b, r;
+	
+	printf ("Digite o primeiro número: ");
+	scanf ("%d", &a);
+	
+	printf ("Digite o segundo número: ");
+	scanf ("%d", &b);
+	
+	r = soma (a, b);
+	
+	printf ("Resultado: %d", r);
+	
+	return 0;
+}
+	
+	
